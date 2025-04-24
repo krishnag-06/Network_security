a6c7a35ae12565edd7df57c7541882fdd7133694
krishnag-06 <krishnagoyal06@icloud.com>
2025-04-24 15:23:29 +0530

    data validation

---
+++ networksecurity/utils/main_utils/utils.py
@@ -0,0 +1,26 @@
+import yaml
+from networksecurity.exceptions.exceptions import CustomException
+from networksecurity.logging.logger import logging
+
+import os
+import sys
+import dill
+import pickle
+
+def read_yaml_file(file_path:str) -> dict:
+    try:
+        with open(file_path, "rb") as yaml_file:
+            return yaml.safe_load(yaml_file)
+    except Exception as e:
+        raise CustomException(e,sys)
+
+def write_yaml_file(file_path:str, content: object, replace: bool = False):
+    try:
+        if replace:
+            if os.path.exists(file_path):
+                os.remove(file_path)
+        os.makedirs(os.path.dirname(file_path),exist_ok= True)
+        with open(file_path, "w") as file:
+            yaml.dump(content, file)
+    except Exception as e:
+        raise CustomException(e,sys)
